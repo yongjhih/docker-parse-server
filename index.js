5491a48d2de57577a93e5773462d6318d32d37c3
Andrew Chen <yongjhih@gmail.com>
2016-02-04 01:25:32 +0800

    Rename vars

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ var api = new ParseServer({
 var app = express();
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
-app.use(mountPath, api);
+var parseHome = process.env.PARSE_HOME || '/parse';
+app.use(parseHome, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
