6e138902e59877b55b34db386d72b1f34eea4217
Andrew Chen <yongjhih@gmail.com>
2016-02-05 22:42:06 +0800

    Update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
+#!/usr/bin/env node
+// ref. parse-server/index.js
+// ref. parse-server/bin/parse-server
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
