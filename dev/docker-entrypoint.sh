6fedfc45f4d1b469ecfc0b48cb86a1d585bd9f07
Andrew Chen <yongjhih@gmail.com>
2016-02-28 00:58:52 +0800

    Add dev image based on ParsePlatform/parse-server instead of parse-server npm

---
+++ dev/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+/etc/init.d/ssh start
+
+if [ ! -d /parse-cloud-code ]; then
+  mkdir -p /parse-cloud-code
+fi
+
+if [ ! -d /parse/cloud ]; then
+  mkdir -p /parse/cloud
+fi
+
+pushd /parse-cloud-code
+git init --bare
+
+cat << EOF > /parse-cloud-code/hooks/post-receive
+#!/bin/bash
+unset GIT_INDEX_FILE
+git --work-tree=/parse/cloud clean -df
+git --work-tree=/parse/cloud checkout -f
+EOF
+
+chown -R git:git /parse-cloud-code
+chown -R git:git /parse/cloud
+chmod a+x /parse-cloud-code/hooks/post-receive
+popd
+
+npm run build && npm start -- --appId $APP_ID --masterKey $MASTER_KEY --serverURL http://localhost:1337/parse
