ed4f07f1738023883408caf0cec1c929db9899bd
Andrew Chen <yongjhih@gmail.com>
2016-02-28 04:34:44 +0800

    dev: Add /parse/lib/parse-cloud-code for nodemon

--- dev/docker-entrypoint.sh
+++ dev/docker-entrypoint.sh
@@ -12,22 +12,29 @@ if [ ! -d /parse/cloud ]; then
   mkdir -p /parse/cloud
 fi
 
+if [ ! -d /parse/lib/parse-cloud-code ]; then
+  mkdir -p /parse/lib/parse-cloud-code
+fi
+
 pushd /parse-cloud-code
 git init --bare
 
 cat << EOF > /parse-cloud-code/hooks/post-receive
 #!/bin/bash
 unset GIT_INDEX_FILE
-git --work-tree=/parse/cloud clean -df
-git --work-tree=/parse/cloud checkout -f
+git --work-tree=/parse/lib/parse-cloud-code clean -df
+git --work-tree=/parse/lib/parse-cloud-code checkout -f
 EOF
 
-chown -R git:git /parse-cloud-code
-chown -R git:git /parse/cloud
 chmod a+x /parse-cloud-code/hooks/post-receive
 popd
 
-ln -snf /parse/cloud /parse/src/parse-cloud-code
+chown -R git:git /parse-cloud-code
+chown -R git:git /parse/cloud
+
 npm run build
+
+cp -a /parse/cloud/* /parse/lib/parse-cloud-code/
+chown -R git:git /parse/lib/parse-cloud-code
 
 npm start -- --appId $APP_ID --masterKey $MASTER_KEY --serverURL http://localhost:1337/parse --cloud /parse/lib/parse-cloud-code/main.js
