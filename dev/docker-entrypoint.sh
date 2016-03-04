f593e41c6b0bbed30adebd05f9a3e44c7a947f31
Andrew Chen <yongjhih@gmail.com>
2016-03-04 19:04:36 +0800

    docker-entrypoint: Avoid duplicated flow

--- dev/docker-entrypoint.sh
+++ dev/docker-entrypoint.sh
@@ -5,27 +5,22 @@ set -e
 /etc/init.d/ssh start > /dev/null
 
 if [ ! -d /parse-cloud-code ]; then
-  mkdir -p /parse-cloud-code > /dev/null
-fi
+    mkdir -p /parse-cloud-code
 
-if [ ! -d /parse/cloud ]; then
-  mkdir -p /parse/cloud > /dev/null
-fi
+    git init --bare /parse-cloud-code > /dev/null
 
-pushd /parse-cloud-code > /dev/null
-git init --bare > /dev/null
-
-cat << EOF > /parse-cloud-code/hooks/post-receive
+    cat << EOF > /parse-cloud-code/hooks/post-receive
 #!/bin/bash
 unset GIT_INDEX_FILE
-git --work-tree=/parse/cloud clean -df
-git --work-tree=/parse/cloud checkout -f
+git --work-tree="${CLOUD_CODE_HOME}" clean -df
+git --work-tree="${CLOUD_CODE_HOME}" checkout -f
 EOF
 
-chown -R git:git /parse-cloud-code > /dev/null
-chown -R git:git /parse/cloud > /dev/null
-chmod a+x /parse-cloud-code/hooks/post-receive > /dev/null
-popd > /dev/null
+    chmod a+x /parse-cloud-code/hooks/post-receive > /dev/null
+
+    chown -R git:git /parse-cloud-code > /dev/null
+    chown -R git:git "$CLOUD_CODE_HOME" > /dev/null
+fi
 
 # Allow update /parse/cloud via git
 sed -i 's#"start": "./bin/parse-server"#"start": "nodemon --watch /parse/cloud ./bin/parse-server"#' package.json > /dev/null
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,20 +4,22 @@ set -e
 
 /etc/init.d/ssh start > /dev/null
 
-mkdir -p /parse-cloud-code
+if [ ! -d /parse-cloud-code ]; then
+    mkdir -p /parse-cloud-code
 
-git init --bare /parse-cloud-code > /dev/null
+    git init --bare /parse-cloud-code > /dev/null
 
-cat << EOF > /parse-cloud-code/hooks/post-receive
+    cat << EOF > /parse-cloud-code/hooks/post-receive
 #!/bin/bash
 unset GIT_INDEX_FILE
 git --work-tree="${CLOUD_CODE_HOME}" clean -df
 git --work-tree="${CLOUD_CODE_HOME}" checkout -f
 EOF
 
-chmod a+x /parse-cloud-code/hooks/post-receive > /dev/null
+    chmod a+x /parse-cloud-code/hooks/post-receive > /dev/null
 
-chown -R git:git /parse-cloud-code > /dev/null
-chown -R git:git "$CLOUD_CODE_HOME" > /dev/null
+    chown -R git:git /parse-cloud-code > /dev/null
+    chown -R git:git "$CLOUD_CODE_HOME" > /dev/null
+fi
 
 exec "$@"
